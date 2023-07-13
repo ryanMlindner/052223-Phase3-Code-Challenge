9a9d472c6b10cf4ad7d6a54dd07480205ac84791
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-07-13 10:55:30 -0600

    challenge completed

--- lib/classes/band.py
+++ lib/classes/band.py
@@ -1,3 +1,37 @@
 from classes.concert import Concert
+
 class Band:
-    pass
+    def __init__(self, name, hometown):
+        self.name = name
+        self.hometown = hometown
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and name != '':
+            self._name = name
+        else:
+            raise Exception
+    
+    @property
+    def hometown(self):
+        return self._hometown
+    @hometown.setter
+    def hometown(self, hometown):
+        if isinstance(hometown, str) and hometown != '':
+            self._hometown = hometown
+        else:
+            raise Exception
+    
+    #why is this a property?
+    @property
+    def concerts(self):
+        return [concert for concert in Concert.all if concert.band == self]
+    
+    def play_in_venue(self, venue, date):
+        Concert(date, self, venue)
+    
+    def all_introductions(self):
+        return [concert.introduction() for concert in Concert.all if concert.band == self]
--- lib/classes/venue.py
+++ lib/classes/venue.py
@@ -1,4 +1,44 @@
 from classes.concert import Concert
 
 class Venue:
- pass
+    def __init__(self, title, city):
+        self.title = title
+        self.city = city
+    
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and title != '':
+            self._title = title
+        else:
+            raise Exception
+    
+    @property
+    def city(self):
+        return self._city
+    @city.setter
+    def city(self, city):
+        if isinstance(city, str) and city != '':
+            self._city = city
+        else:
+            raise Exception
+    
+    def concerts(self):
+        return [concert for concert in Concert.all if concert.venue == self]
+    
+    def bands(self):
+        return [concert.band for concert in Concert.all if concert.venue == self]
+    
+    #this deliverable feels weirdly worded and I'm not sure I did it correctly, but
+    #all the tests work and technically it does what the description says
+    #would make much more sense if there was some concrete way to order the concerts (i.e. times)
+    #maybe I'm looking at it wrong, let me know please
+    def concert_on(self, date):
+        first_concert = None
+        concerts = [concert for concert in Concert.all if concert.date == date]
+        print(concerts)
+        if len(concerts) != 0:
+            first_concert = concerts[-1]
+        return first_concert
